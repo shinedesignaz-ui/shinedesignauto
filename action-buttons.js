da2c095f3162867a140899cd282c7f42103db57b
shinedesignaz-ui <shinedesignaz@gmail.com>
2025-09-21 02:36:13 -0700

    commit

--- action-buttons.js
+++ action-buttons.js
@@ -101,7 +101,7 @@
           </svg>
           <span>Calculator</span>
         </button>
-        <a href="https://booknow.shinedesignauto.com/" class="footer-item">
+        <a href="https://booknow.shinedesignaz.com/" class="footer-item">
           <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
           </svg>
@@ -141,7 +141,7 @@
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z"></path>
         </svg>
       </button>
-      <a href="https://booknow.shinedesignauto.com/" class="floating-btn primary">
+      <a href="https://booknow.shinedesignaz.com/" class="floating-btn primary">
         <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
         </svg>
